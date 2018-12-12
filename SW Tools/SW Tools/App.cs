c26ec2cbd4cd3f83a9bd3549aff591085d3b6931
Thomas Østgulen <35105200+tostgulen@users.noreply.github.com>
2018-12-12 09:02:01 +0100

    Start
    Its a start of a project.

---
+++ SW Tools/SW Tools/App.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System.Windows.Media.Imaging;
+
+namespace SW_Tools
+{
+    public class App : IExternalApplication
+    {
+        // define a method that will create our tab and button
+
+        static void AddRibbonPanel(UIControlledApplication application)
+        {
+            // Create a custom ribbon tab
+            String tabName = "SW Tools";
+            application.CreateRibbonTab(tabName);
+
+            // Add a new ribbon panel
+            RibbonPanel ribbonPanel = application.CreateRibbonPanel(tabName, "Tools");
+
+            // Get dll assembly path
+            string thisAssemblyPath = Assembly.GetExecutingAssembly().Location;
+
+            // create push button for CurveTotalLength
+            PushButtonData b1Data = new PushButtonData(
+                "cmdFirstButton",
+                "Total" + System.Environment.NewLine + "  Length  ", 
+                thisAssemblyPath,
+                "SW_Tools.FirstApp");
+
+            PushButton pb1 = ribbonPanel.AddItem(b1Data) as PushButton;
+            pb1.ToolTip = "This is the tooltip for the functin";
+            BitmapImage pb1Image = new BitmapImage(new Uri("pack://application:,,,/SW Tools;component/Resources/iconStar.png"));
+            pb1.LargeImage = pb1Image;
+        }
+
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            // do nothing
+            // return result.succeeded
+        }
+
+        public Result OnStartup(UIControlledApplication application)
+        {
+            // call our method that will load up our toolbar
+            // return result.succeeded
+
+        }
+    }
+}
---
+++ SW Tools/SW Tools/CurveTotalLength.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.Attributes;
+
+namespace TotalLength
+{
+
+    [Transaction(TransactionMode.Manual)]
+ 
+
+    class CurveTotalLenght : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            // Get application and document objects
+
+            try
+            {
+                // Implement Selection Filter to select curves
+
+                // Measure their total length
+
+                // Return a message window that displays total length to user
+
+                // Assuming that everything went right return Result.Succeeded
+                return Result.Succeeded;
+            }
+            // This is where we "catch" potential errors and define how to deal with them
+            catch (Autodesk.Revit.Exceptions.OperationCanceledException)
+            {
+                // If user decided to cancel the operation return Result.Canceled
+                return Result.Cancelled;
+            }
+            catch (Exception ex)
+            {
+                // If something went wrong return Result.Failed
+                message = ex.Message;
+                return Result.Failed;
+            }
+
+            throw new NotImplementedException();
+        }
+    }
+}
